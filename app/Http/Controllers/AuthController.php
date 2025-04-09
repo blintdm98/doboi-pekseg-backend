e32c53ac354467aa3af9fa36c4a144b92c2c1f91
Bokor Attila <attila.bokor@leadingsoft.eu>
2025-04-09 11:40:02 +0300

    login to use auth attempt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,24 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-
         $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        $user = User::where('email', $credentials['email'])->first();
-
-        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+        if (!Auth::attempt($credentials)) {
             return response()->json(['message' => 'Hibás bejelentkezési adatok'], 401);
         }
+
+        $user = Auth::user();
 
         $token = $user->createToken('mobile-token')->plainTextToken;
 
