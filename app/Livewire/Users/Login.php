e7b4c8623abf1582938e7964b592afa6b51a319a
Bálint Ádám <blintdm98@gmail.com>
2025-06-18 12:53:21 +0300

    login with admin role only

--- app/Livewire/Users/Login.php
+++ app/Livewire/Users/Login.php
@@ -19,18 +19,28 @@ class Login extends Component
 
     public function save()
     {
-        $this->validate();
+        $this->validate([
+            'user_name' => 'required|exists:users|max:255',
+            'password'  => 'required|max:255',
+        ]);
 
         if (!auth()->attempt([
             'user_name' => $this->user_name,
             'password'  => $this->password,
         ])) {
             $this->addError('common', __('user.your_credentials_invalid'));
-        } else {
-            session()->regenerate();
-
-            return redirect('/');
+            return;
         }
+
+        // ⛔️ Role ellenőrzés itt
+        if (auth()->user()->role !== 'admin') {
+            auth()->logout(); // kiléptetés, ha nem admin
+            $this->addError('common', 'Csak admin felhasználók léphetnek be.');
+            return;
+        }
+
+        session()->regenerate();
+        return redirect('/');
     }
 
     public function render()
