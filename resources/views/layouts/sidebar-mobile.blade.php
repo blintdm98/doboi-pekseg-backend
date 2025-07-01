cd0bcc995dcbaba44a9efcddb89ffafd490d8a03
Bálint Ádám <blintdm98@gmail.com>
2025-07-01 08:24:42 +0300

    logout button restyling

--- resources/views/layouts/sidebar-mobile.blade.php
+++ resources/views/layouts/sidebar-mobile.blade.php
@@ -11,7 +11,7 @@
     class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"
 ></div>
 <aside
-    class="fixed inset-y-0 z-20 shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 lg:hidden"
+    class="fixed inset-y-0 z-20 shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 lg:hidden flex flex-col"
     x-show="isSideMenuOpen"
     x-transition:enter="transition ease-in-out duration-150"
     x-transition:enter-start="opacity-0 transform -translate-x-20"
@@ -22,19 +22,33 @@
     @click.away="closeSideMenu"
     @keydown.escape="closeSideMenu"
 >
-    <div class="py-4 text-gray-500 dark:text-gray-400">
-        <a
-            class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200 mb-6"
-            href="#"
-        >
-            {{config('app.name')}}
-        </a>
-        <div class="mt-6">
-            <ul>
-                @foreach($navbarItems as $item)
-                    @include('layouts.menu-item',$item)
-                @endforeach
-            </ul>
+    <div class="py-4 text-gray-500 dark:text-gray-400 flex flex-col h-full">
+        <div class="flex-1">
+            <a
+                class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200 mb-6"
+                href="#"
+            >
+                {{config('app.name')}}
+            </a>
+            <div class="mt-6">
+                <ul>
+                    @foreach($navbarItems as $item)
+                        @include('layouts.menu-item',$item)
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+        <!-- Kijelentkezés gomb a mobile sidebar alján -->
+        <div class="mt-auto pt-4 border-t border-gray-200 dark:border-gray-700">
+            <a
+                href="{{route('logout')}}"
+                class="border-l-[3px] ease-in-out duration-200 flex items-center gap-2 border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-gray-200"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                </svg>
+                <span class="text-sm font-medium">Kijelentkezés</span>
+            </a>
         </div>
     </div>
 </aside>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,22 +6,36 @@
     <aside
         class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 lg:flex flex-col justify-between shrink-0"
     >
-        <div class="py-4 text-gray-500 dark:text-gray-400">
-            <a
-                class="text-lg font-bold text-gray-800 dark:text-gray-200"
-                href="/"
-            >
-                <div class="ml-6">
-                    <div>
-                        <span>{{config('app.name')}}</span>
+        <div class="py-4 text-gray-500 dark:text-gray-400 flex flex-col h-full">
+            <div class="flex-1">
+                <a
+                    class="text-lg font-bold text-gray-800 dark:text-gray-200"
+                    href="/"
+                >
+                    <div class="ml-6">
+                        <div>
+                            <span>{{config('app.name')}}</span>
+                        </div>
                     </div>
-                </div>
-            </a>
-            <ul class="mt-6">
-                @foreach($navbarItems as $item)
-                    @include('layouts.menu-item',$item)
-                @endforeach
-            </ul>
+                </a>
+                <ul class="mt-6">
+                    @foreach($navbarItems as $item)
+                        @include('layouts.menu-item',$item)
+                    @endforeach
+                </ul>
+            </div>
+            <!-- Kijelentkezés gomb a sidebar alján -->
+            <div class="mt-auto pt-4 border-t border-gray-200 dark:border-gray-700">
+                <a
+                    href="{{route('logout')}}"
+                    class="border-l-[3px] ease-in-out duration-200 flex items-center gap-2 border-transparent px-4 py-3 text-gray-500 hover:border-gray-100 hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-gray-200"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                    </svg>
+                    <span class="text-sm font-medium">Kijelentkezés</span>
+                </a>
+            </div>
         </div>
     </aside>
     @include('layouts.sidebar-mobile')
